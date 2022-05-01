7e2c9894debd6c2ab662c66eebcb78fa94cf72eb
Fernando Melo <fernando.jsts@gmail.com>
2022-05-01 13:32:30 -0300

    Contact Button (feat): Add button that redirect to contact page

---
+++ src/components/ContactButton/ContactButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Button } from "szot-ui-experimental";
+  import { link } from "svelte-spa-router";
+</script>
+
+<div class="button">
+  <a href="/contact" use:link>
+    <Button size="large">Entrar em contato</Button>
+  </a>
+</div>
+
+<style lang="scss">
+  .button {
+    --szot-button-background-color: linear-gradient(90deg, #354b45, #308472);
+    --szot-button-color: white;
+    --szot-button-border-radius: 15px;
+  }
+</style>
