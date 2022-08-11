900c85e5eb63977adbb52d1b363a7351aa84e29b
Fernando Jorge <fernando.jsts@gmail.com>
2022-08-11 09:22:13 -0300

    :lipstick: Addapt style on app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -38,13 +38,6 @@
 <style>
   main {
     text-align: center;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    margin: 5rem auto 0;
   }
 </style>
