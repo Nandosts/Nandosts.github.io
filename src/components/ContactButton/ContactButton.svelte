ce7502e1a9e92a251ba4173c857459d3b22c6446
Fernando Jorge <fernando.jsts@gmail.com>
2022-08-13 11:12:24 -0300

    :lipstick: Avoid button breaking line

--- src/components/ContactButton/ContactButton.svelte
+++ src/components/ContactButton/ContactButton.svelte
@@ -17,6 +17,8 @@
 
 <style lang="scss">
   .button {
+    white-space: nowrap;
+
     --szot-button-background-color: linear-gradient(90deg, #354b45, #308472);
     --szot-button-color: white;
     --szot-button-border-radius: 15px;
