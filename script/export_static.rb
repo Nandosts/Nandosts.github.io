05bcea66532d75917a0828973be1dda1ee1a7122
Fernando Melo <65089035+Nandosts@users.noreply.github.com>
2026-01-14 12:52:31 -0300

    fix(test): corrige teste de controller e script de exportação estática

--- script/export_static.rb
+++ script/export_static.rb
@@ -1,3 +1,4 @@
+require_relative '../config/environment'
 require "fileutils"
 
 export_dir = "public_static"
--- test/controllers/sites_controller_test.rb
+++ test/controllers/sites_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SitesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get site_url("1")
+    get site_path(id: Project.all.first.id)
     assert_response :success
   end
 end
