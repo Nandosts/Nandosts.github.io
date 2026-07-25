b5b3c07de81ea44525be25fc02ffb30d66cb73c5
Fernando Melo <65089035+Nandosts@users.noreply.github.com>
2026-07-25 10:52:34 -0300

    configura meta tags de SEO dinamicas, open graph e url canonica

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def titulo_pagina
+    if content_for?(:title)
+      "#{content_for(:title)} | Fernando Melo"
+    else
+      "Fernando Melo | Desenvolvedor Full Stack & Engenheiro de Software"
+    end
+  end
+
+  def descricao_meta_pagina
+    if content_for?(:meta_description)
+      content_for(:meta_description)
+    else
+      "Desenvolvedor Full Stack e Engenheiro de Software com mais de 5 anos de experiência na construção de sistemas web escaláveis em Ruby on Rails, JavaScript e arquiteturas modernas."
+    end
+  end
+
+  def url_canonica_pagina
+    caminho_base = request.path == "/" ? "" : request.path
+    "https://nandosts.github.io#{caminho_base}"
+  end
 end
+
